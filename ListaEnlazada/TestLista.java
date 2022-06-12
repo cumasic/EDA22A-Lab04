506344caa48747c3f01a8f88a95145425cc92f1f
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-12 17:36:17 -0500

    Se agregaron las funciones peorCaso y insertSort

--- ListaEnlazada/TestLista.java
+++ ListaEnlazada/TestLista.java
@@ -3,4 +3,12 @@ package ListaEnlazada;
 public class TestLista {
     public static void main(String[]args) {
     }
+    public static ListaEnlazada<Integer> generarPeorCaso(int num) {
+		ListaEnlazada<Integer> peorCaso = new ListaEnlazada<Integer>();
+		for(int i = 0 ;i < num; i++)
+			peorCaso.insert(num-i);
+		return peorCaso;
+	}
+    public static void insertionSort(ListaEnlazada<Integer> a) {
+    }
 }
