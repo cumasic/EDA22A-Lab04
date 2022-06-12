dd0c7abc5c94104c3ee82ca860c715cdddf881d5
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-12 17:48:38 -0500

    Se añadieron las funciones a DobleListaEnlazada

--- DobleListaEnlazada/DobleListaEnlazada.java
+++ DobleListaEnlazada/DobleListaEnlazada.java
@@ -7,6 +7,51 @@ public class DobleListaEnlazada<E> {
 	public DobleListaEnlazada() {
 		this.root = null;
 	}
+    public boolean isEmpty(){
+		return this.root == null;
+	}
+	public int length() {
+		return tamaño;
+	}
+	public void insert(E x) {
+		if(isEmpty()) {
+			this.root = new Node<E>(x);
+		}
+		else {
+			Node<E> aux = this.root;
+			Node<E> aux2 = new Node<E>(x);
+			for(;aux.getNext() != null; aux = aux.getNext());
+			aux2.setPrevious(aux);
+			aux.setNext(aux2); 
+		}
+		tamaño++;
+	}
+	public void insertPos(E x,int indice) {
+		if(indice<=tamaño) {
+	        if(indice==0) {
+	        	Node<E> aux = new Node<E>(x);
+	        	this.root.setPrevious(aux);
+	        	this.root = new Node<E>(x,this.root);
+	        }
+	        else if(indice==tamaño) {
+	        	Node<E> aux1=this.get(indice-1);
+	        	aux1.setNext(new Node<E>(x,aux1.getNext()));
+	        	Node<E> aux2=this.get(indice);
+	        	aux2.setPrevious(new Node<E>(x,null,aux2.getPrevious()));
+	        }
+	        else {
+	        	Node<E> aux1=this.get(indice-1);
+	        	aux1.setNext(new Node<E>(x,aux1.getNext()));
+	        	Node<E> aux2=this.get(indice+1);
+	        	aux2.setPrevious(new Node<E>(x,null,aux2.getPrevious()));
+	        }
+	        tamaño++;
+	    }
+	}
+	public E getInd(int indice) {
+	    Node<E> aux = this.get(indice);
+	    return aux.getData();
+	}
     public Node<E> get(int indice) {
 	    Node<E> aux = root;
 	    for(int i=0;i<indice;i++)
@@ -33,5 +78,12 @@ public class DobleListaEnlazada<E> {
 	        }
 	        tamaño--;
 	    }
+	}
+    public String toString() {
+		String str = "";
+		for(Node<E> aux = this.root; aux != null;aux = aux.getNext()) {
+			str += aux.getData() + ", ";
+		}
+		return str;
 	}
 }
