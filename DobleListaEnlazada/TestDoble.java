7fbe52f225fa0d76fd965017610f7eb04025ca19
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-06-12 17:51:40 -0500

    Se implemento la función insertionSort

--- DobleListaEnlazada/TestDoble.java
+++ DobleListaEnlazada/TestDoble.java
@@ -2,6 +2,15 @@ package DobleListaEnlazada;
 
 public class TestDoble {
     public static void main(String[]args){
+        DobleListaEnlazada<Integer> list = new DobleListaEnlazada<Integer>();
+        list.insert(21);
+		list.insert(40);
+		list.insert(17);
+		list.insert(7);
+		list.insert(35);
+        System.out.println(list);
+        insertionSort(list);
+        System.out.println(list);
     }
     public static DobleListaEnlazada<Integer> generarPeorCaso(int num) {
 		DobleListaEnlazada<Integer> peorCaso = new DobleListaEnlazada<Integer>();
@@ -10,5 +19,19 @@ public class TestDoble {
 		return peorCaso;
 	}
     public static void insertionSort(DobleListaEnlazada<Integer> a) {
+        int key;
+		int i;
+		for (int j=1; j<a.length(); j=j+1) {
+			key = a.getInd(j);
+			//Insertar A[j] en la secuencia ordenada A[1..j-1]
+			i=j-1;
+			while(i>-1 && a.getInd(i)>key) {
+				a.remove(i+1);
+				a.insertPos(a.getInd(i), i+1);
+				i = i-1;
+			}
+			a.remove(i+1);
+			a.insertPos(key, i+1);
+		}
     }
 }
